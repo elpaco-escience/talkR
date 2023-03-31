d93c6f6f39ab5c699328d683370bef843d03f5b3
mdingemanse <mark@vormdicht.nl>
2023-03-31 15:00:36 +0200

    new add_lines() function

---
+++ R/add_lines.R
@@ -0,0 +1,25 @@
+#' Add lines
+#'
+#' @param data dataset to divide into lines
+#' @param extract_length total length of extract in ms
+#' @param window_size length of line in ms
+#'
+#' @return data set with 'line' column added
+#' @export
+#'
+add_lines <- function(data,extract_length=600000,window_size=60000) {
+
+  window_breaks <- as.integer(c(0:round(extract_length/window_size)) * window_size)
+
+  data <- data |>
+    dplyr::mutate(end = end - min(begin), # reset timestamps to start from 0
+           begin = begin - min(begin),
+           line = cut(begin,window_breaks,right=F,labels=F)) |>
+    tidyr::drop_na(line) |>
+    dplyr::group_by(line) |>
+    dplyr::mutate(begin0 = begin - min(begin), # reset timestamps to 0 for each new line
+           end0 = end - min(begin)) |>
+    dplyr::ungroup()
+
+  return(data)
+}
