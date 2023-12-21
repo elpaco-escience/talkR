9e608050d79221a67db65804ff3724bc4e43b0b1
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-12-21 13:00:10 +0100

    Add calculation for conversation timing properties

--- R/check.R
+++ R/check.R
@@ -13,6 +13,22 @@ check_columns <- function(data, columns){
 }
 
 
+#' Verify that timing columns are numeric and likely indicate milliseconds.
+#'
+#' @param column vector with timing information
+#' @param name name of the column
+#'
+#' @return nothing, but throws an error if the column is not numeric and warns if the column may not indicate milliseconds
+check_time <- function(column, name){
+  if(!is.numeric(column)){
+    stop("Column `",name,"` must be numeric.")
+  }
+  if(max_na(column) < 5*60*1000){ # expected conversation duration is more than 5 minutes, corresponding to 5*60*1000 milliseconds
+    warning("Verify that column `",name,"` is in milliseconds.")
+  }
+}
+
+
 #' Check the presence of talkr-workflow columns in the dataset.
 #'
 #' Uses check_columns() to check for:
@@ -21,9 +37,17 @@ check_columns <- function(data, columns){
 #' - participant
 #' - utterance
 #' - source
+#' - uid
+#'
+#' Verifies that begin and end columns are numeric, and likely indicate milliseconds.
 #'
 #' @param data dataset to check
 check_talkr <- function(data) {
-  required_cols <- c("begin", "end", "participant", "utterance", "source")
+  required_cols <- c("begin", "end", "participant", "utterance", "source", "uid")
   check_columns(data, required_cols)
+
+  numeric_cols <- c("begin", "end")
+  for (col in numeric_cols){
+    check_time(data[[col]], col)
+  }
 }
