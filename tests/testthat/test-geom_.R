74709a82e2b7c730ea24bede723a24e58860f744
Barbara Vreede <b.vreede@esciencecenter.nl>
2024-01-09 15:38:45 +0100

    add geom tests

---
+++ tests/testthat/test-geom_.R
@@ -0,0 +1,32 @@
+load("testdata.Rda")
+data <- init(testdata)
+
+test_that("applying geometries yields expected datasets", {
+  p0 <- data |>
+    ggplot2::ggplot(aes(x = end, y = participant))
+
+  d0 <- ggplot2::layer_data(p0)
+  expect_equal(d0$x, data$end)
+  expect_equal(max(d0$y), 23)
+
+  p1 <- p0 +
+    geom_turn(aes(
+      begin = begin,
+      end = end))
+
+  d1 <- ggplot2::layer_data(p1)
+  expect_equal(d1$end, data$end)
+  expect_equal(d1$begin, data$begin)
+  expect_equal(max(d1$y), 23)
+
+  tokens <- tokenize(data)
+
+  p2 <- p1 +
+  geom_token(data = tokens,
+                      aes(x = relative_time,
+                          y = participant,
+                          color = rank))
+
+  d2 <- ggplot2::layer_data(p2, 2)
+  expect_equal(d2$x, tokens$relative_time)
+})
