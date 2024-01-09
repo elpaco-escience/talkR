1320da0c8ac0def93b0e53d5e3292a383843a60f
Barbara Vreede <b.vreede@esciencecenter.nl>
2024-01-09 17:13:26 +0100

    Add additional options to default turn plot

--- R/plot_turns_tokens.R
+++ R/plot_turns_tokens.R
@@ -1,25 +1,38 @@
-#' Plot conversation
+#' Plot a stretch of conversation
+#'
+#' Utterances are plotted as grey boxes, with frequently occurring tokens overlaid as colored dots.
 #'
 #' @param data a talkr dataset
-#' @param sourcecount number of the source to plot (defaults to 1)
+#' @param time_start start time of the plot in seconds (defaults to 0)
+#' @param time_end end time of the plot in seconds (defaults to 60)
+#' @param maxrank maximum rank of tokens to plot (defaults to 10)
+#' @param source name or number of the source to plot (defaults to 1, plotting the first source in the data)
 #'
 #' @return plot object
 #' @export
-plot_turns_tokens <- function(data, sourcecount = 1){
+plot_turns_tokens <- function(data,
+                              time_start = 0,
+                              time_end = 60,
+                              maxrank = 10,
+                              source = 1){
   check_talkr(data)
 
-  window_length = 60 * 1000 # one minute, in milliseconds. The duration of a single line.
-
-  sourcecount <- min(sourcecount, length(unique(data$source)))
-  source <- unique(data$source)[sourcecount]
-
+  if(is.numeric(source)){
+    sourcenr <- min(source, length(unique(data$source)))
+    source <- unique(data$source)[sourcenr]
+  }
   data <- data[data$source == source,]
-  data <- data[data$end < window_length,]
 
   tokens <- data |>
     tokenize()
+  tokens <- tokens[tokens$rank < maxrank,]
 
-  tokens <- tokens[tokens$rank < 10,]
+  time_start <- time_start * 1000
+  time_end <- time_end * 1000
+  data <- data[data$begin > time_start & data$end < time_end,]
+
+  uids_included <- unique(data$uid)
+  tokens <- tokens[tokens$uid %in% uids_included,]
 
   p <- data |>
     ggplot2::ggplot(aes(x = end, y = participant)) +
