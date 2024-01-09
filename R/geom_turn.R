fa1d9909d1d9b532730981f9f915a6f4a6c32428
Barbara Vreede <b.vreede@esciencecenter.nl>
2024-01-09 17:12:40 +0100

    Update geom documentation with inherited parameters

--- R/geom_turn.R
+++ R/geom_turn.R
@@ -1,16 +1,12 @@
 #' Show turn-taking in visualized conversations
 #'
-#' @param mapping Set of aesthetic mappings created by aes(). If specified and inherit.aes = TRUE (the default), it is combined with the default mapping at the top level of the plot. You must supply mapping if there is no plot mapping.
-#' @param data The data to be displayed in this layer. There are three options: If NULL, the default, the data is inherited from the plot data as specified in the call to ggplot().
-#' @param stat The statistical transformation to use on the data for this layer, either as a ggproto Geom subclass or as a string naming the stat stripped of the stat_ prefix (e.g. "count" rather than "stat_count")
-#' @param position Position adjustment, either as a string naming the adjustment (e.g. "jitter" to use position_jitter), or the result of a call to a position adjustment function. Use the latter if you need to change the settings of the adjustment.
-#' @param na.rm If FALSE, the default, missing values are removed with a warning. If TRUE, missing values are silently removed.
-#' @param height The height of the rectangles representing turns
-#' @param show.legend logical. Should this layer be included in the legends? NA, the default, includes if any aesthetics are mapped. FALSE never includes, and TRUE always includes. It can also be a named logical vector to finely select the aesthetics to display.
-#' @param inherit.aes If FALSE, overrides the default aesthetics, rather than combining with them. This is most useful for helper functions that define both data and aesthetics and shouldn't inherit behaviour from the default plot specification, e.g. borders().
-#' @param ... Other arguments passed on to layer(). These are often aesthetics, used to set an aesthetic to a fixed value, like colour = "red" or size = 3. They may also be parameters to the paired geom/stat.
+#' @param mapping Set of aesthetic mappings created by `ggplot2::aes()`.
+#'    Requires specification of `begin` and `end` of turns. Inherits from the default mapping at the
+#'    top level of the plot, if `inherit.aes` is set to `TRUE` (the default).
+#' @inheritParams ggplot2::layer
+#' @inheritParams ggplot2::geom_rect
+#' @param height The height of the turn-taking rectangles
 #' @export
-#' @rdname geom_turn
 geom_turn <- function(mapping = NULL, data = NULL,
                       stat = "identity", position = "identity",
                       ..., na.rm = FALSE, height = 0.8, show.legend = NA, inherit.aes = TRUE) {
