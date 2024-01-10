4abaa58f5c6706d258e25826d3a1e1bd1c28b7a7
Barbara Vreede <b.vreede@esciencecenter.nl>
2024-01-10 10:40:31 +0100

    update plot customization options

--- R/plot_turns_tokens.R
+++ R/plot_turns_tokens.R
@@ -3,16 +3,16 @@
 #' Utterances are plotted as grey boxes, with frequently occurring tokens overlaid as colored dots.
 #'
 #' @param data a talkr dataset
-#' @param time_start start time of the plot in seconds (defaults to 0)
-#' @param time_end end time of the plot in seconds (defaults to 60)
+#' @param begin start time of the plot in seconds (defaults to 0)
+#' @param duration duration of the plot in seconds (defaults to 60)
 #' @param maxrank maximum rank of tokens to plot (defaults to 10)
 #' @param source name or number of the source to plot (defaults to 1, plotting the first source in the data)
 #'
 #' @return plot object
 #' @export
 plot_turns_tokens <- function(data,
-                              time_start = 0,
-                              time_end = 60,
+                              begin = 0,
+                              duration = 60,
                               maxrank = 10,
                               source = 1){
   check_talkr(data)
@@ -27,22 +27,24 @@ plot_turns_tokens <- function(data,
     tokenize()
   tokens <- tokens[tokens$rank < maxrank,]
 
-  time_start <- time_start * 1000
-  time_end <- time_end * 1000
+  time_start <- begin * 1000
+  time_end <- time_start + duration * 1000
   data <- data[data$begin > time_start & data$end < time_end,]
 
   uids_included <- unique(data$uid)
   tokens <- tokens[tokens$uid %in% uids_included,]
 
   p <- data |>
-    ggplot2::ggplot(aes(x = end, y = participant)) +
+    ggplot2::ggplot(aes(
+      x = .data$end,
+      y = .data$participant)) +
     talkr::geom_turn(aes(
-      begin = begin,
-      end = end)) +
+      begin = .data$begin,
+      end = .data$end)) +
     talkr::geom_token(data = tokens,
-                      aes(x = relative_time,
-                          y = participant,
-                          color = rank)) +
+                      aes(x = .data$relative_time,
+                          y = .data$participant,
+                          color = .data$rank)) +
     talkr::theme_turnPlot() +
     ggplot2::xlab("time (ms)") +
     ggplot2::ggtitle(source)
