1984bfa9dea7a8b50f1c790002bd5294da4ba7bc
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2024-08-20 12:04:10 +0200

    Minor improvements (#73)
    * Bump roxygen2 to latest version
    
    * Update docs
    
    * Update authors list
    
    * Update .gitignore
    
    * Ignore CITATION.cff on build
    
    * Capitalize title field
    
    * Lists Suggests at the end
    
    This way the file reflects the Depends > Imports > Suggests hierarchy
    
    * Add ifadv as a dependency
    
    Ranks as a Suggests type dependency
    
    * Enable GitHub action for branch 61-cran
    
    * Perform check --as-cran
    
    * Revert "Perform check --as-cran"
    
    This reverts commit 5c35d9f709c5454744e3c19d65e469ac4aa2ef2a.
    
    * Remove devtools from description
    
    * Update .Rbuildignore
    
    * Update .github/workflows/R-CMD-check.yaml
    
    * Update .gitignore

--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -1,12 +1,4 @@
 ## set up the test environment
-# Install ifadv and devtools only if required
-if (!requireNamespace("ifadv")){
-  if (!requireNamespace("devtools")){
-    install.packages("devtools")
-  }
-  devtools::install_github("elpaco-escience/ifadv")
-}
-
 data <- ifadv::ifadv
 
 test_that("summary reports are accurate", {
