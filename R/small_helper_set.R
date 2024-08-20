8090a874e901e1460d3edfd79aa171cbc2b6256c
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2024-08-20 12:13:43 +0200

    Remove unused auxiliary functions

--- R/small_helper_set.R
+++ R/small_helper_set.R
@@ -1,8 +1,5 @@
-`%notin%` <- function(x,y) !(x %in% y)
 mean_na <- function(x) mean(x, na.rm = T)
-median_na <- function(x) stats::median(x, na.rm= T)
 min_na <- function(x) min(x, na.rm = T)
 max_na <- function(x) max(x, na.rm = T)
-sd_na <- function(x) stats::sd(x, na.rm = T)
 sum_na <- function(x) sum(x, na.rm = T)
 
