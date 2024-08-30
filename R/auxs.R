561cf3c5e326031067377ac291b9e805e57c7dd6
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2024-08-30 14:57:44 +0200

    Greatly simplify the download process
    For some reason, native R data files (.rda) are less comfortable for R than the good-old csv. I pushed a csv copy of the rda file to https://github.com/elpaco-escience/ifadv/tree/csv, and that's the one we are using now.

--- R/auxs.R
+++ R/auxs.R
@@ -4,29 +4,13 @@
 #' Available in the public repository:
 #' https://github.com/elpaco-escience/ifadv
 #'
-#' @param destfile (default = "ifadv.rda") Destination file
-#' @param clean_after (default = TRUE) Cleans the downloaded file after using it
+#' This function requires an internet connection.
 #'
-#' @return A tibble containing the IFADV dataset
-#' @importFrom utils download.file
+#' @param source (default = "https://raw.githubusercontent.com/elpaco-escience/ifadv/csv/data/ifadv.csv")
+#'
+#' @return A data frame containing the IFADV dataset
 #' @export
 #'
-get_ifadv <- function(destfile = "ifadv.rda", clean_after = TRUE) {
-
-  # We are forced to download the data into a temporary file
-  # and load the file into memory locally
-  # The snippet below makes this easy to do
-  if(file.exists(destfile)) {
-    stop("The file already exists. Choose a different destfile")
-  } else {
-      file.create(destfile) # Create a dummy file
-  }
-  # Let's not forget cleaning our mess after ourselves
-  on.exit(if(clean_after & file.exists(destfile)) file.remove(destfile))
-
-  download.file("https://raw.github.com/elpaco-escience/ifadv/main/data/ifadv.rda", destfile, quiet = TRUE)
-
-  e <- new.env()
-  load(destfile, envir = e) # Load it to the function environment ...
-  return(e[["ifadv"]]) # ... and return it as an output
+get_ifadv <- function(source="https://raw.githubusercontent.com/elpaco-escience/ifadv/csv/data/ifadv.csv") {
+  return(utils::read.csv(source))
 }
--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -2,7 +2,6 @@
 testdata <- get_ifadv()
 
 test_that("summary reports are accurate", {
-  testdata <- get_ifadv(destfile = "../../data/ifadv.rda")
   expect_snapshot(
     report_stats(
       testdata
